90c1b60f392268bcdb901726d9da5bff516352a0
Elton Silva <eltoncarsilva73@gmail.com>
2024-09-29 20:53:35 -0300

    drawing a pixel

--- doom.py
+++ doom.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
 running = True
 
@@ -10,9 +10,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill("purple")
-    pygame.display.flip()
+    screen.fill("white")
+    pygame.draw.rect(screen, "red", pygame.Rect(400,400, 4, 4))
 
+    pygame.display.flip()
 
     clock.tick(60) 
 

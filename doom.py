d13fbc8e01410391427d99b2a3e35feccaa30a9d
Elton Silva <eltoncarsilva73@gmail.com>
2024-09-30 01:00:29 -0300

    minor refactoring and fixes

--- doom.py
+++ doom.py
@@ -6,8 +6,8 @@ screen = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
 running = True
 
-SH = 200
-SW = 200
+SCREEN_HEIGHT = 200
+SCREEN_WIDTH = 200
 
 pixelSize = 4
 cameraX =  70
@@ -19,21 +19,21 @@ cameraL = 0
 pixels = []
 
 def SetUp():
-    for x in range(SW):
+    for x in range(SCREEN_WIDTH):
         pixels.append([])
-        for y in range(SH):
+        for y in range(SCREEN_HEIGHT):
             pixels[x].append("black")
 
 def Clear():
-    for x in range(SW):
-        for y in range(SH):
-            pixels[x][y] = "white"
+    for x in range(SCREEN_WIDTH):
+        for y in range(SCREEN_HEIGHT):
+                pixels[x][y] = "black"
 
 def Render():
-    if cameraX > 0 and cameraX < SW and cameraY> 0 and cameraY < SH:
+    if cameraX > 0 and cameraX < SCREEN_WIDTH and cameraY> 0 and cameraY < SCREEN_HEIGHT:
         pixels[int(cameraX)][int(cameraY)] = "red"
-    for x in range(SW):
-        for y in range(SH):
+    for x in range(SCREEN_WIDTH):
+        for y in range(SCREEN_HEIGHT):
             pygame.draw.rect(screen, pixels[x][y], pygame.Rect(x * pixelSize, y * pixelSize, pixelSize, pixelSize))
 
 def DrawWall(x1 : int , x2 : int, b1 : int,  b2 : int, t1 : int, t2 : int):
@@ -41,18 +41,27 @@ def DrawWall(x1 : int , x2 : int, b1 : int,  b2 : int, t1 : int, t2 : int):
     dyt = t2 - t1
     dx = x2 - x1
 
-    if dx == 0:
-        dx = 1
+    if dx == 0: dx = 1
     xs = x1
+
+    if x1 < 1:  x1 = 1
+    if x2 < 1: x2 = 1
+    if x1 > SCREEN_WIDTH - 1: x1 = SCREEN_WIDTH - 1
+    if x2 > SCREEN_WIDTH - 1: x2 = SCREEN_WIDTH - 1 
 
     for x in range(int(x2 - x1)):
         xx = int(x + x1)
-        y1 = int(dyb * (xx - xs + 0.5) / dx + b1)
-        y2 = int(dyt * (xx - xs + 0.5) / dx + t1)
+        y1 = int(dyb * (xx - xs) / dx + b1)
+        y2 = int(dyt * (xx - xs) / dx + t1)
+
+        if y1 < 1: y1 = 1
+        if y2 < 1: x2 = 1
+        if y1 > SCREEN_HEIGHT - 1: y1 = SCREEN_HEIGHT - 1
+        if y2 > SCREEN_HEIGHT - 1: y2 = SCREEN_HEIGHT - 1 
 
         for y in range(int(y2 - y1)):
             yy = y + y1
-            if xx > 0 and xx< SW and yy > 0 and yy < SH:
+            if xx > 0 and xx< SCREEN_WIDTH and yy > 0 and yy < SCREEN_HEIGHT:
                 pixels[xx][yy] = "yellow"
 
 
@@ -77,21 +86,24 @@ def Draw3D():
     
     worldY[0] = y1 * cs + x1 * sn
     worldY[1] = y2 * cs + x2 * sn
-    worldY[2] = worldY[0] + 40
-    worldY[3] = worldY[1] + 40
+    worldY[2] = worldY[0]
+    worldY[3] = worldY[1]
 
     worldZ[0] = 0 - cameraZ + ((cameraL * worldY[0]) / 32)
     worldZ[1] = 0 - cameraZ + ((cameraL * worldY[1]) / 32)
+    worldZ[2] = worldZ[0] + 40
+    worldZ[3] = worldZ[1] + 40
 
-    worldX[0] = worldX[0] * 200 / worldY[0] + (SW / 2)
-    worldY[0] = worldZ[0] * 200 / worldY[0] + (SH / 2)
-    worldX[1] = worldX[1] * 200 / worldY[1] + (SW / 2)
-    worldY[1] = worldZ[1] * 200 / worldY[1] + (SH / 2)
 
-    worldX[2] = worldX[2] * 200 / worldY[2] + (SW / 2)
-    worldY[2] = worldZ[2] * 200 / worldY[2] + (SH / 2)
-    worldX[3] = worldX[3] * 200 / worldY[3] + (SW / 2)
-    worldY[3] = worldZ[3] * 200 / worldY[3] + (SH / 2)
+    worldX[0] = worldX[0] * 200 / worldY[0] + (SCREEN_WIDTH / 2)
+    worldY[0] = worldZ[0] * 200 / worldY[0] + (SCREEN_HEIGHT / 2)
+    worldX[1] = worldX[1] * 200 / worldY[1] + (SCREEN_WIDTH / 2)
+    worldY[1] = worldZ[1] * 200 / worldY[1] + (SCREEN_HEIGHT / 2)
+
+    worldX[2] = worldX[2] * 200 / worldY[2] + (SCREEN_WIDTH / 2)
+    worldY[2] = worldZ[2] * 200 / worldY[2] + (SCREEN_HEIGHT / 2)
+    worldX[3] = worldX[3] * 200 / worldY[3] + (SCREEN_WIDTH / 2)
+    worldY[3] = worldZ[3] * 200 / worldY[3] + (SCREEN_HEIGHT / 2)
 
     DrawWall(worldX[0], worldX[1], worldY[0], worldY[1], worldY[2], worldY[3])
 

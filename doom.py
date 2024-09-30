fd7295afc27881215b96b011a70e73b99c6ad758
Elton Silva <eltoncarsilva73@gmail.com>
2024-09-29 21:08:56 -0300

    created logic to move point on screen

--- doom.py
+++ doom.py
@@ -5,13 +5,33 @@ screen = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
 running = True
 
+pixelSize = 4
+cameraX = pixelSize
+cameraY = pixelSize
+cameraZ = pixelSize
+cameraA = 0
+cameraL = 0
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill("white")
-    pygame.draw.rect(screen, "red", pygame.Rect(400,400, 4, 4))
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        cameraY -= pixelSize
+    if keys[pygame.K_s]:
+        cameraY += pixelSize
+    if keys[pygame.K_a]:
+        cameraX -= pixelSize
+    if keys[pygame.K_d]:
+        cameraX += pixelSize
+
+
+    pygame.draw.rect(screen, "red", pygame.Rect(cameraX,cameraY, pixelSize, pixelSize))
 
     pygame.display.flip()
 

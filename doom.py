91662b7afd2fb1a52d264649b7cd44dc57f2f535
Elton Silva <eltoncarsilva73@gmail.com>
2024-09-29 21:46:44 -0300

    render pespective line

--- doom.py
+++ doom.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 pygame.init()
 screen = pygame.display.set_mode((800, 800))
@@ -12,6 +13,36 @@ cameraZ = pixelSize
 cameraA = 0
 cameraL = 0
 
+def Draw3D():
+    worldX = [0,0,0,0]
+    worldY = [0,0,0,0]
+    worldZ = [0,0,0,0]
+
+    cs = math.radians(math.cos(cameraA))
+    sn = math.radians(math.sin(cameraA))
+    
+    x1 = 40 - cameraX
+    y1 = 10 - cameraY
+
+    x2 = 40 - cameraX
+    y2 = 290 - cameraY
+
+    worldX[0] = x1 - y1
+    worldX[1] = x2 - y2 
+
+    worldY[0] = y1 + x1
+    worldY[1] = y2 + x2
+
+    worldZ[0] = 0 - cameraZ + ((cameraL * worldY[0]) / 32)
+    worldZ[1] = 0 - cameraZ + ((cameraL * worldY[1]) / 32)
+
+    worldX[0] = worldX[0] * 200 / worldY[0] + 200
+    worldY[0] = worldZ[0] * 200 / worldY[0] + 200
+    worldX[1] = worldX[1] * 200 / worldY[1] + 200
+    worldY[1] = worldZ[1] * 200 / worldY[1] + 200
+
+    pygame.draw.rect(screen, "blue", pygame.Rect(worldX[0], worldY[0], pixelSize, pixelSize))
+    pygame.draw.rect(screen, "blue", pygame.Rect(worldX[1], worldY[1], pixelSize, pixelSize))
 
 while running:
     for event in pygame.event.get():
@@ -30,11 +61,14 @@ while running:
     if keys[pygame.K_d]:
         cameraX += pixelSize
 
-
     pygame.draw.rect(screen, "red", pygame.Rect(cameraX,cameraY, pixelSize, pixelSize))
+    Draw3D()
 
     pygame.display.flip()
 
     clock.tick(60) 
 
 pygame.quit()
+
+
+

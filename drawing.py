be927fd65660183b6f3c0013cca1373ef38d05e1
Elton Silva <eltoncarsilva73@gmail.com>
2024-09-30 22:53:16 -0300

    fix method draw line

--- drawing.py
+++ drawing.py
@@ -35,16 +35,29 @@ def Render():
             pygame.draw.rect(screen, pixels[x][y], pygame.Rect(x * pixelSize, y * pixelSize, pixelSize, pixelSize))
 
 def DrawLine(p1x : int, p1y : int, p2x : int, p2y : int):
-    directionX = p2x - p1x
-    directionY = p2y - p1y	
+    firstPointY = p1y
+    endPointY = p2y
+    firstPointX = p1x
+    endPointX = p2x
 
-    for y in range(p1y, p2y + 1):
-        yy = y - p1y
-        x = int(yy / directionY * directionX) + p1x
-        
+    if p1y > p2y : 
+        firstPointY = p2y 
+        endPointY = p1y
+        firstPointX = p2x
+        endPointX = p1x
+
+    directionX = endPointX - firstPointX
+    directionY = endPointY - firstPointY
+
+    y = firstPointY
+    while y <= endPointY:
+        yy = y - firstPointY
+        x = int(yy / directionY * directionX + 0.7) + firstPointY
+
         if x > 0 and x < SCREEN_WIDTH and y > 0 and y < SCREEN_HEIGHT:
             pixels[x][y] = "yellow"
-    
+        y+= 1
+
     if p1x > 0 and p1x < SCREEN_WIDTH and p1y > 0 and p1y < SCREEN_HEIGHT:
         pixels[p1x][p1y] = "red"
     if p2x > 0 and p2x < SCREEN_WIDTH and p2y > 0 and p2y < SCREEN_HEIGHT:
@@ -62,7 +75,7 @@ while running:
 
     screen.fill("white")
     Clear()
-    DrawLine(100, 100, 159, 159)
+    DrawLine(100, 70, 25, 25)
     Render()
 
     pygame.display.flip()
